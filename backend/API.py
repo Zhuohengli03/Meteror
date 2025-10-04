bff914142fb407c8b5a65243aff78c362fb3929f
Zhuohengli03 <zhuohengli03@gmail.com>
2025-10-04 14:54:09 -0400

    new

--- backend/API.py
+++ backend/API.py
@@ -1,11 +1,13 @@
 import requests
+import datetime
 
 URL = "https://api.nasa.gov/neo/rest/v1/neo/browse?"
 
 sparams = {
     "api_key": "CLePa8TOYYjIoOJZ1VyN42dQ6rvp9ZscdJJCBp5k",
+    "start_date": datetime.datetime.now().strftime("%Y-%m-%d"),
+    "end_date": "2000-01-01",
 }
-
 response = requests.get(URL, params=sparams)
 
 print(response.json())
